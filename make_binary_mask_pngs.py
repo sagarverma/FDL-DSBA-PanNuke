effb81f830dc9f47a39c9e9917e17244ea71066b
Sagar <versag@Sagars-Mac-mini.local>
2020-12-04 00:10:46 +0100

    bugfix and simple testing

--- make_binary_mask_pngs.py
+++ make_binary_mask_pngs.py
@@ -3,12 +3,16 @@ import numpy as np
 import cv2
 
 def process_fold(fold):
-    images = np.load('data/' + fold + '/images/fold1/images.npy')
-    types = np.load('data/' + fold + '/images/fold1/types.npy')
-    masks = np.load('data/' + fold + '/masks/fold1/masks.npy')
+    # images = np.load('data/' + fold + '/images/fold1/images.npy')
+    # types = np.load('data/' + fold + '/images/fold1/types.npy')
+    # masks = np.load('data/' + fold + '/masks/fold1/masks.npy')
+    images = np.zeros(100,32,32,3)
+    masks = np.zeros(100,32,32,6)
 
-    os.makedirs('data/' + fold + '_images')
-    os.makedirs('data/' + fold + '_masks')
+    if not os.path.exists('data/' + fold + '_images'):
+        os.makedirs('data/' + fold + '_images')
+    if not os.path.exists('data/' + fold + '_masks'):
+        os.makedirs('data/' + fold + '_masks')
 
     for i in range(0, images.shape[0]):
         cv2.imwrite(os.path.join('data/' + fold + '_images', str(i).zfill(5) + '.png'),
