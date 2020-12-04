ec3e2692e1a79cade94360fdf5f3c4af2e883af6
sagarverma <sagar15056@iiitd.ac.in>
2020-12-05 00:43:01 +0100

    rle decode

--- create_rle.py
+++ create_rle.py
@@ -3,18 +3,32 @@ import os
 from PIL import Image
 import numpy as np
 
-def rle_encode(mask_image):
-    pixels = mask_image.flatten()
-    # We avoid issues with '1' at the start or end (at the corners of
-    # the original image) by setting those pixels to '0' explicitly.
-    # We do not expect these to be non-zero for an accurate mask,
-    # so this should not harm the score.
-    pixels[0] = 0
-    pixels[-1] = 0
-    runs = np.where(pixels[1:] != pixels[:-1])[0] + 2
-    runs[1::2] = runs[1::2] - runs[:-1:2]
-    return runs
+def rle_encode(img):
+    '''
+    img: numpy array, 1 - mask, 0 - background
+    Returns run length as string formated
+    '''
+    pixels = img.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
 
+def rle_decode(mask_rle, shape):
+    '''
+    mask_rle: run-length as string formated (start length)
+    shape: (height,width) of array to return
+    Returns numpy array, 1 - mask, 0 - background
+
+    '''
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape)
 
 
 def create_rles():
@@ -24,12 +38,15 @@ def create_rles():
     with open('answer_key.csv', 'a') as f:
         f.write('img,rle_mask\n')
         for index, i in enumerate(os.listdir(dir)):
-            print('{}/{}'.format(index, N))
+            # print('{}/{}'.format(index, N))
 
             mask = Image.open(dir + i)
             mask = np.array(mask)
 
             enc = rle_encode(mask // 255)
-            f.write('{},{}\n'.format(i, ' '.join(map(str, enc))))
+            f.write('{},{}\n'.format(i, enc))
+            dec = rle_decode(enc, (256, 256))
+            if np.sum(dec - enc) > 0:
+                print(N)
 
 create_rles()
